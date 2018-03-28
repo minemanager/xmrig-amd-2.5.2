611e9ac9202f20a22a4fdf86e3f342a1a182175e
clwaddel <30387984+clwaddel@users.noreply.github.com>
2018-03-28 19:21:46 -0400

    first commit

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@ apt-get install git build-essential cmake libuv1-dev libmicrohttpd-dev
 cd xmrig
 mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=/usr/local/bin/miners/xmrig-2.5.2/ -DWITH_LIBCPUID=OFF -DWITH_HTTPD=OFF ..
+cmake -DCMAKE_INSTALL_PREFIX=/usr/local/bin/miners/xmrig-amd-2.5.2/ -DWITH_LIBCPUID=OFF -DWITH_HTTPD=OFF ..
 make
 make install
